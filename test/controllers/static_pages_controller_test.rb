74ca3be05c2cf2168258f586dc96b7f886d43d5f
Huzaifa Raghav <windowpane1712@gmail.com>
2018-08-08 18:08:15 +0800

    Add contact page, change root route, add root route test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,22 +7,43 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   #Title tests
+  test "should get root" do
+    get '/'
+    assert_response :success
+
+    assert_select "title", "Home | #{@base_title}"
+    assert_select "h1", "Home"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+
     assert_select "title", "Home | #{@base_title}"
+    assert_select "h1", "Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+
     assert_select "title", "Help | #{@base_title}"
+    assert_select "h1", "Help"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+
     assert_select "title", "About | #{@base_title}"
+    assert_select "h1", "About"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+
+    assert_select "title", "Contact | #{@base_title}"
+    assert_select "h1", "Contact"
+  end
 end
