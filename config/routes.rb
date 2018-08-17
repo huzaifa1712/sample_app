74591deddc16dea1bfaa7b2ac64193edbf261b3b
huzaifa1712 <windowpane1712@gmail.com>
2018-08-17 16:12:48 +0800

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/users', to: 'users#index'
   post '/signup', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
