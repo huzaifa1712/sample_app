a20abb95479b2106f03f9ee0a36a1e8dcbd02691
huzaifa1712 <windowpane1712@gmail.com>
2018-08-17 23:22:23 +0800

    Change sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    render 'new'
+  end
+
+  def destroy
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params)
+      #Log user in and redirect to show page
+      redirect_to user
+    else
+      #Show errors
+      render 'new'
+    end
+  end
 end
