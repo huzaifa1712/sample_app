96a7b3187615a64ca02e07708c89305df1e0f44b
huzaifa1712 <windowpane1712@gmail.com>
2018-08-17 16:38:35 +0800

    Refactor flash messages, additional test for flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,6 +5,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  test "valid signup information" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count', 1 do
+      post signup_path, params:{
+        user:{
+          name: "Valid Name",
+          email: "user@valid.com",
+          password: "validpassword",
+          password_confirmation: "validpassword"
+        }
+      }
+    end
+    follow_redirect! #follow the redirect to show user
+    assert_template 'users/show'
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action="/signup"]'
@@ -19,6 +36,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation ul li', 4
+    assert_select 'div.alert'
   end
-
 end
